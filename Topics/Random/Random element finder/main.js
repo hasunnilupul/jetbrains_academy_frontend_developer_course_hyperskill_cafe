ed96ac79493338d4198055d82830f21d6a3a3858
Hasun Nilupul <hasunnilupul16@gmail.com>
2022-04-10 22:25:20 +0530

    Random

---
+++ Topics/Random/Random element finder/main.js
@@ -0,0 +1,4 @@
+function getRomanNumber(array) {
+    const index = Math.floor(Math.random() * (array.length - 1)) + 1;
+    return array[index];
+}
---
+++ Topics/Random/Simple randomizer/main.js
@@ -0,0 +1,3 @@
+function getRandom() {
+    return Math.floor(Math.random() * (5 - 0)) + 0;
+}
